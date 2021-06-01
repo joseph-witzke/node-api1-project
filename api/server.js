9e88932d8cd9f096658d3705ff43a0f2713d0bb1
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-01 07:58:00 -0600

    Finished MVP

--- api/server.js
+++ api/server.js
@@ -34,6 +34,63 @@ server.get('/api/users/:id', async (req, res) => {
     
 })
 
+server.post('/api/users', async (req, res) => {
+    try {
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
+            })
+        } else {
+            const newUser = await Users.insert(req.body)
+            res.status(201).json(newUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error while saving the user to the database",
+            error: err.message,
+        })
+    }
+})
+
+server.put('/api/users', async (req, res) => {
+    const { id } = req.params
+    const { body } = req
+    try {
+        if (!updatedUser) {
+            res.status(404).json({
+                message: `"The user with the specified ID ${id} does not exist"`
+            })
+        } else {
+            const updatedUser = await Users.update(id, body)
+            res.status(200).json(updatedUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The user information could not be modified",
+            error: err.message,
+        })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+    Users.remove(id)
+        .then(deleted => {
+            if (!deleted) {
+                res.status(404).json({
+                    message: `"The user with the specified ID ${id} does not exist"`
+                })
+            } else {
+                res.json(deleted)
+            }
+        })
+        .catch (err => {
+            res.status(500).json({
+                message: "The user could not be removed",
+                error: err.message,
+            })
+        })
+})
 
 
 
