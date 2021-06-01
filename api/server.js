6091a28928baaff0e37707059b3b288aa868964e
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-01 07:12:11 -0600

    GET all is working

--- api/server.js
+++ api/server.js
@@ -1,9 +1,31 @@
-// BUILD YOUR SERVER HERE
+// Imports
 const express = require('express')
 const Users = require('./users/model')
 
+//Instance of Express App
 const server = express()
 
+//Global Middleware
 server.use(express.json())
 
+//Endpoints
+server.get('/api/users', (req, res) => {
+    Users.find()
+     .then(users => {
+         res.json(users)
+     })
+     .catch(err => {
+         res.status(500).json({ message: "The users information could not be retrieved",
+         error: err.message,
+        })
+     })
+})
+
+
+
+
+
+
+
+//Exposing the server to other modules
 module.exports = server; // EXPORT YOUR SERVER instead of {}
